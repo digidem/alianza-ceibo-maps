901f0116af477a781be33e31445903f0b3bb458f
Gregor MacLennan <gmaclennan@digital-democracy.org>
2017-12-18 13:17:26 -0800

    Add crowdin incontext editing support

--- components/App.js
+++ components/App.js
@@ -3,14 +3,44 @@ import qs from 'querystring'
 import { BrowserRouter, Route } from 'react-router-dom'
 import { IntlProvider, addLocaleData } from 'react-intl'
 import esLocaleData from 'react-intl/locale-data/es'
+import assign from 'object-assign'
 
 import Main from './Main'
+import esStrings from '../messages/es.json'
+import xxStrings from '../messages/xx.json'
 
-const locale = qs.parse(window.location.search.replace(/^\?/, '')).lang || 'en'
+const translations = {
+  en: {},
+  es: formatMessages(esStrings, 'es'),
+  xx: formatMessages(xxStrings, 'xx')
+}
+
+function formatMessages (strings, locale) {
+  return Object.keys(strings).reduce((messages, id) => {
+    messages[id] = strings[id].message
+    return messages
+  }, {})
+}
+
+const query = qs.parse(window.location.search.replace(/^\?/, ''))
+let locale = query.lang || 'en'
 addLocaleData(esLocaleData)
 
+if (typeof query.translate !== 'undefined') {
+  locale = 'xx'
+  window._jipt = [['project', 'alianza-ceibo-maps']]
+  const script = document.createElement('script')
+  script.type = 'text/javascript'
+  script.src = '//cdn.crowdin.com/jipt/jipt.js'
+  document.head.appendChild(script)
+  // This pseudo locale is used for crowdin incontext translation
+  // https://support.crowdin.com/in-context-localization/
+  const pseudoLocale = assign({}, esLocaleData[0], {locale: 'xx'})
+  addLocaleData([pseudoLocale])
+}
+
 const App = () => (
-  <IntlProvider locale={locale}>
+  <IntlProvider locale={locale} messages={translations[locale]}>
     <BrowserRouter>
       <Route path='/:nation?/:area?/:community?' render={({match, history, location}) => (
         <Main
